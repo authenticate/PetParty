bbf7d51b4063fa2da1377fbbc8ecc68a72f8f05c
James Lammlein <j.lammlein@gmail.com>
2014-10-18 15:19:11 -0400

    Adding some initial files to get started.

---
+++ PetParty.lua
@@ -0,0 +1,36 @@
+--
+-- Pet Party
+-- Copyright 2014 James Lammlein
+--
+-- This file is part of Pet Party.
+--
+-- Pet Party is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- any later version.
+--
+-- Pet Party is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Pet Party.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+-- Pet Party localization.  Use US English as the default localization.
+PetParty.L = PetParty.L_enUS;
+
+--
+-- TODO: Debugging code.  Remove later.
+--
+
+PetParty_Frame:Show();
+--PetParty_Frame:Hide();
+
+local frame = CreateFrame("Frame");
+frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+frame:SetScript("OnEvent", function()
+    print(PetParty.L["Hello World!"]);
+    end
+);
---
+++ PetParty_UI.lua
@@ -0,0 +1,28 @@
+--
+-- Pet Party
+-- Copyright 2014 James Lammlein
+--
+-- This file is part of Pet Party.
+--
+-- Pet Party is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- any later version.
+--
+-- Pet Party is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Pet Party.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+--
+-- TODO: Debugging code.  Remove later.
+--
+
+function PetParty.OnLoadPetParty_Frame()
+    print("Hello world from the frame!");
+    return;
+end
