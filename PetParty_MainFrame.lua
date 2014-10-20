d7736da9428b2d962547ee3dade4c9463bc14fca
James Lammlein <j.lammlein@gmail.com>
2014-10-20 16:04:34 -0400

    Adding support for dragging to resize the frame.

--- PetParty_MainFrame.lua
+++ PetParty_MainFrame.lua
@@ -18,6 +18,15 @@
 -- along with Pet Party.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+--
+-- Constants.
+--
+
+local FRAME_MAXIMUM_WIDTH = 0;
+local FRAME_MAXIMUM_HEIGHT = 0;
+local FRAME_MINIMUM_WIDTH = 384;
+local FRAME_MINIMUM_HEIGHT = 512;
+
 -- Called when the main frame starts dragging.
 function PetParty.OnDragStartMainFrame()
     PetParty_MainFrame:StartMoving();
@@ -25,6 +34,16 @@ end
 
 -- Called when the main frame stops dragging.
 function PetParty.OnDragStopMainFrame()
+    PetParty_MainFrame:StopMovingOrSizing();
+end
+
+-- Called when the main frame's resize button starts dragging.
+function PetParty.OnDragStartMainFrameButtonResize()
+    PetParty_MainFrame:StartSizing();
+end
+
+-- Called when the main frame's resize button stops dragging.
+function PetParty.OnDragStopMainFrameButtonResize()
     PetParty_MainFrame:StopMovingOrSizing();
 end
 
@@ -49,10 +68,14 @@ end
 function PetParty.OnLoadMainFrame()
     PetParty_MainFrame:SetClampedToScreen(true);
     PetParty_MainFrame:SetUserPlaced(true);
+    PetParty_MainFrame:SetMaxResize(FRAME_MAXIMUM_WIDTH, FRAME_MAXIMUM_HEIGHT);
+    PetParty_MainFrame:SetMinResize(FRAME_MINIMUM_WIDTH, FRAME_MINIMUM_HEIGHT);
     
     PetParty_MainFrame:RegisterEvent("ADDON_LOADED");
     PetParty_MainFrame:RegisterEvent("PLAYER_LOGOUT");
     PetParty_MainFrame:RegisterForDrag("LeftButton");
+    
+    PetParty_MainFrame_Button_Resize:RegisterForDrag("LeftButton");
     
     PetParty.CreateContentAndScrollFrames();
 end
