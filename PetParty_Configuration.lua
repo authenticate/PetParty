91529fc9b9b4045865ae067e09a3cfaddd6c3df6
James Lammlein <j.lammlein@gmail.com>
2015-02-24 12:47:33 -0500

    Updating version numbers.

--- PetParty_Configuration.lua
+++ PetParty_Configuration.lua
@@ -26,7 +26,7 @@ PetParty = {};
 --
 
 PetParty.ADDON_NAME = "PetParty";
-PetParty.ADDON_VERSION = "1.4";
+PetParty.ADDON_VERSION = "1.5";
 
 PetParty.ABILITY_GROUPS_PER_PET = 3;
 PetParty.ABILITIES_PER_ABILITY_GROUP = 2;
